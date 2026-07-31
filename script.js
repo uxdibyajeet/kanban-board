38aa3f1ac27e6ddf1daf9046206d7cab71a667b5
uxdibyajeet <ux.dibyajeet@gmail.com>
2026-07-31 08:27:55 +0000

    task cards can be created via add form

--- script.js
+++ script.js
@@ -1,32 +1,140 @@
-// generates the UI for columns
-const generateColums = (() => {
-  const colNames = ["backlogs", "to dos", "in progress", "done"];
-  const boardContainer = document.querySelector(".board");
-  colNames.forEach((col) => {
-    const colHeader = document.createElement("div");
-    const column = document.createElement("div");
-    const colName = document.createElement("h2");
+const kanbanInit = (() => {
+  // factor: create tasks
+  let tasks = [];
+  const createTasks = (title, details, duedate, priority) => {
+    return { title, details, duedate, priority };
+  };
 
-    colName.textContent = col;
+  // get tasks
+  const getTasks = () => {
+    return tasks;
+  };
 
-    colName.setAttribute("class", "col-header-text");
-    colHeader.setAttribute("class", "col-header");
-    colHeader.appendChild(colName);
-    column.setAttribute("class", "column");
-    column.appendChild(colHeader);
-    boardContainer.appendChild(column);
-  });
+  // adds task add form
+  const addForm = document.querySelector("#add-form");
+
+  const addTask = (event) => {
+    event.preventDefault();
+
+    const taskName = addForm.elements["task_name"]?.value;
+    const taskDueDate = addForm.elements["due_date"]?.value;
+    const taskPriority = addForm.elements["task_priority"]?.value;
+    const taskDescription = addForm.elements["task_description"]?.value;
+
+    const newTask = createTasks(
+      taskName,
+      taskDescription,
+      taskDueDate,
+      taskPriority,
+    );
+
+    tasks.push(newTask);
+    addForm.reset();
+    addModal.close();
+    updateTotalTasksBadge();
+    generateCard();
+  };
+
+  // event listners
+  addForm.addEventListener("submit", addTask);
+
+  return { createTasks, addForm, getTasks };
 })();
+
+const updateTotalTasksBadge = () => {
+  const badge = document.querySelector("#count-badge");
+  const totalTasks = kanbanInit.getTasks().length;
+  badge.setAttribute("class", "info-badge");
+  badge.textContent = totalTasks;
+};
 
 // Add task modal
 const addTaskBtn = document.querySelector("#add-task");
 const addModal = document.querySelector("#add-modal");
 addTaskBtn.addEventListener("click", (event) => {
+  kanbanInit.addForm.reset();
   addModal.showModal();
 });
 
-// factor: create tasks
-let tasks = [];
-const createTasks = (title, details, duedate, priority) => {
-  return [title, details, duedate, priority];
+// close add modal via close btn
+const closeAddModalBtn = document.querySelector("#close-add-modal");
+closeAddModalBtn.addEventListener("click", () => {
+  addModal.close();
+});
+
+// generates the UI for columns
+const generateColums = (() => {
+  const colNames = ["backlogs", "to dos", "in progress", "done"];
+  const boardContainer = document.querySelector(".board");
+
+  colNames.forEach((col) => {
+    const colHeader = document.createElement("div");
+    const column = document.createElement("div");
+    const colName = document.createElement("h2");
+    const btn = document.createElement("button");
+    const cardContainer = document.createElement("div");
+    // const badge = document.createElement("span");
+
+    colName.textContent = col;
+
+    // adds icons to the button element
+    btn.innerHTML = `
+        <svg class="icon" viewBox="0 0 24 24" fill="none">
+            <use href="#img-plus"></use>
+        </svg>`;
+
+    btn.classList.add("btn", "secondary", "btn-icon-only", "small", "hidden");
+    colName.setAttribute("class", "col-header-text");
+    // badge.setAttribute("class", "info-badge");
+    // colName.appendChild(badge);
+    colHeader.setAttribute("class", "col-header");
+    colHeader.append(colName, btn);
+    column.setAttribute("class", "column");
+    cardContainer.setAttribute("id", "card-container");
+    cardContainer.setAttribute("class", "card-container");
+
+    column.append(colHeader, cardContainer);
+    boardContainer.appendChild(column);
+
+    colHeader.addEventListener("mouseenter", () => {
+      btn.classList.remove("hidden");
+    });
+    colHeader.addEventListener("mouseleave", () => {
+      btn.classList.add("hidden");
+    });
+  });
+})();
+
+// generate cards
+const generateCard = () => {
+  const cardArr = kanbanInit.getTasks();
+  const board = document.querySelector(".board");
+  const cardContainer = board.querySelectorAll("#card-container");
+  cardContainer[0].innerHTML = "";
+
+  cardArr.forEach((card) => {
+    const cardBody = document.createElement("div");
+    const title = document.createElement("h3");
+    const desc = document.createElement("p");
+    const priorityBar = document.createElement("span");
+
+    title.textContent = card.title;
+    desc.textContent = card.details;
+
+    cardBody.append(title, desc, priorityBar);
+
+    // adding classes
+    cardBody.setAttribute("class", "card");
+    priorityBar.setAttribute("class", "priority-bar");
+    cardContainer[0].appendChild(cardBody);
+
+    // adding utility class to indicate task priority
+    if (card.priority === "high") {
+      priorityBar.classList.add("high");
+    } else if (card.priority === "medium") {
+      priorityBar.classList.add("medium");
+    } else {
+      priorityBar.classList.add("low");
+    }
+  });
 };
